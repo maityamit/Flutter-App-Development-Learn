95c6468892dac53e941461943edc977df1103185
Amit Maity <maityamit308@gmail.com>
2022-04-18 00:46:44 +0530

    Update ui_example_02.dart

--- 20. 20th Day Simple UI/widget/ui_example_02.dart
+++ 20. 20th Day Simple UI/widget/ui_example_02.dart
@@ -37,7 +37,7 @@ class _UIEXAmple20DayState extends State<UIEXAmple20Day> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(Icons.add_box_outlined,size: 35,color: Colors.black,),
+                      Icon(Icons.add_box_outlined,size: 33,color: Colors.black,),
                       SizedBox(
                         width: 10,
                       ),
@@ -56,11 +56,28 @@ class _UIEXAmple20DayState extends State<UIEXAmple20Day> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  margin: EdgeInsets.only(left: 15,top: 15),
-                  child: CircleAvatar(
-                    radius: 40,
-                    backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/74618071?v=4"),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 15,top: 15),
+                        width: 75,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 3,
+                              color: Colors.orange
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          image: DecorationImage(
+                              image: NetworkImage("https://avatars.githubusercontent.com/u/74618071?v=4")
+                          ),
+                        ),
+                      )
+                    ],
                   ),
+
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -145,76 +162,137 @@ class _UIEXAmple20DayState extends State<UIEXAmple20Day> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/273669019_4740095462755518_7366621361474719244_n.webp?stp=c0.420.1080.1080a_dst-jpg_e35_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=6lsGqOiqqDUAX_V0Ml9&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT-LQK9eItp6-CVUVHp4UFrd_57-1TTIVhNzQF07yUWz3w&oe=625EA19D&_nc_sid=276363"),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("Traveling")
-                  ],
+
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 3,
+                              color: Colors.cyanAccent
+                          ),
+                          color: Colors.red,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          image: DecorationImage(
+                              image: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/273669019_4740095462755518_7366621361474719244_n.webp?stp=c0.420.1080.1080a_dst-jpg_e35_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=6lsGqOiqqDUAX_V0Ml9&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT-LQK9eItp6-CVUVHp4UFrd_57-1TTIVhNzQF07yUWz3w&oe=625EA19D&_nc_sid=276363")
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("Traveling")
+                    ],
+                  ),
+
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/272801382_263371755925204_7151276267244709091_n.webp?stp=c0.420.1080.1080a_dst-jpg_e35_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=w8Ms1TBBeKwAX99f_Jj&tn=oiSBlktS6qx_usOD&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT-Juvud19jwD2HTdlKFmu41lmjZBsG70AJmIm7StnlFEg&oe=625E6969&_nc_sid=276363"),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("Some Special")
-                  ],
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 3,
+                              color: Colors.orange
+                          ),
+                          color: Colors.red,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          image: DecorationImage(
+                              image: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/272801382_263371755925204_7151276267244709091_n.webp?stp=c0.420.1080.1080a_dst-jpg_e35_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=w8Ms1TBBeKwAX99f_Jj&tn=oiSBlktS6qx_usOD&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT-Juvud19jwD2HTdlKFmu41lmjZBsG70AJmIm7StnlFEg&oe=625E6969&_nc_sid=276363")
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("Some Special")
+                    ],
+                  ),
+
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/263265765_1244265556059845_8545142196522598564_n.jpg?stp=c0.420.1080.1080a_dst-jpg_e35_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=SPTOjrZOVYcAX8_cYMN&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT8DNKthp2QfV4CdIfVVziddsLYEgVGYMx8FXsU-K9FncQ&oe=625ECEAD&_nc_sid=276363"),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("First Year")
-                  ],
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 3,
+                              color: Colors.yellow
+                          ),
+                          color: Colors.red,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          image: DecorationImage(
+                              image: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/263265765_1244265556059845_8545142196522598564_n.jpg?stp=c0.420.1080.1080a_dst-jpg_e35_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=SPTOjrZOVYcAX8_cYMN&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT8DNKthp2QfV4CdIfVVziddsLYEgVGYMx8FXsU-K9FncQ&oe=625ECEAD&_nc_sid=276363")
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("First Year")
+                    ],
+                  ),
+
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/272003146_326452572692026_5176224149378092094_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=MKdm5yGVs4AAX8T6ZxJ&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT8s7n70ZEXT5g4fgWIVxqEncw2Diro9HemOPv7Y8e1uUA&oe=62631803&_nc_sid=276363"),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("LeetCode")
-                  ],
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 3,
+                              color: Colors.red
+                          ),
+                          color: Colors.red,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          image: DecorationImage(
+                              image: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/272003146_326452572692026_5176224149378092094_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=MKdm5yGVs4AAX8T6ZxJ&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT8s7n70ZEXT5g4fgWIVxqEncw2Diro9HemOPv7Y8e1uUA&oe=62631803&_nc_sid=276363")
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("LeetCode")
+                    ],
+                  ),
+
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/226206850_205753348045109_3008692744087582046_n.jpg?stp=c0.247.640.640a_dst-jpg_e15_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=gChdfzUrdcYAX9zw9GT&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT__xY2EzjlxiFEg53FySISDiamubsLR6ERr4vi0wNAxSQ&oe=625EE188&_nc_sid=276363"),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("Freinds")
-                  ],
-                )
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 3,
+                              color: Colors.blue
+                          ),
+                          color: Colors.green,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          image: DecorationImage(
+                              image: NetworkImage("https://instagram.fdel11-2.fna.fbcdn.net/v/t51.2885-15/226206850_205753348045109_3008692744087582046_n.jpg?stp=c0.247.640.640a_dst-jpg_e15_s150x150&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=gChdfzUrdcYAX9zw9GT&edm=ANmP7GQBAAAA&ccb=7-4&oh=00_AT__xY2EzjlxiFEg53FySISDiamubsLR6ERr4vi0wNAxSQ&oe=625EE188&_nc_sid=276363")
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("Freinds")
+                    ],
+                  ),
+
+                ),
               ],
             ),
             SizedBox(
